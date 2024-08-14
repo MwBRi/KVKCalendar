a0cb43be23141c07755f532ecac38785b4c53581
MwBRi <137848062+MwBRi@users.noreply.github.com>
2024-08-14 19:44:02 +0900

    Update DayPadCell.swift
    a new modifier that change ScrollWeekView header layout

--- Sources/KVKCalendar/DayPadCell.swift
+++ Sources/KVKCalendar/DayPadCell.swift
@@ -20,6 +20,35 @@ final class DayPadCell: DayCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        if (style.isWeekHeaderVertical) {
+            var titleFrame = frame
+            titleFrame.origin.x = 0
+            titleFrame.origin.y = 0
+            titleFrame.size.height = titleFrame.height > heightTitle ? heightTitle : titleFrame.height / 2 - 10
+            titleLabel.frame = titleFrame
+    
+            var dateFrame = frame
+            dateFrame.size.height = frame.height > heightDate ? heightDate : frame.height / 2
+            dateFrame.size.width = heightDate
+            dateFrame.origin.y = titleFrame.height
+            dateFrame.origin.x = (frame.width / 2) - (dateFrame.width / 2)
+            dotView.frame = dateFrame
+            dateLabel.frame = CGRect(origin: .zero, size: dateFrame.size)
+    
+            dotView.addSubview(dateLabel)
+            contentView.addSubview(titleLabel)
+            contentView.addSubview(dotView)
+    
+            if let radius = style.headerScroll.dotCornersRadius {
+                dotView.setRoundCorners(style.headerScroll.dotCorners, radius: radius)
+            } else {
+                let value = dotView.frame.width / 2
+                dotView.setRoundCorners(style.headerScroll.dotCorners, radius: CGSize(width: value, height: value))
+            }
+
+            return
+        }
         
         titleLabel.textAlignment = .right
         dateLabel.textAlignment = .center
